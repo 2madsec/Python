98d697a7c7e4ea4f15e556adeb0dafab1126d9f4
Madboy <91026247+2madsec@users.noreply.github.com>
2022-03-13 21:07:26 +0200

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -2,7 +2,7 @@
 
 # This is a Deauth Attacking Script#
 # Disclaimer: This script is for educational purposes only.  Do not use against any network that you don't own or have authorization to test on.
-# Made by Madboy
+# Made by Nyx
 
 
 import os
